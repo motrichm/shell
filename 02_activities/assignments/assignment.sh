41ef77ec16d95afe459be95fbab422ff740a8d95
Mari Motrich <mmotrich@adm.utoronto.ca>
2026-02-08 09:48:13 -0500

    try 4

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -50,12 +50,12 @@ mkdir event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 #one more change
-cp  /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/raw/server*.log /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed/server_logs/
+cp  ./data/raw/server*.log ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
 
-cp  /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/raw/*user*.log /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed/user_logs/
-cp  /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/raw/*event*.log /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed/event_logs/
+cp  ./data/raw/*user*.log ./data/processed/user_logs/
+cp  ./data/raw/*event*.log ./data/processed/event_logs/
 
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
