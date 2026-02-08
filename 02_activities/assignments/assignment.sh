aadd30d2290353e5343d1bf46e4f67a0dee281ae
Mari Motrich <mmotrich@adm.utoronto.ca>
2026-02-08 09:50:56 -0500

    try #5

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -39,14 +39,7 @@ mv ./rawdata ./data/raw
 ls -la ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-cd newproject
-cd data
-
-mkdir processed
-cd processed
-mkdir server_logs
-mkdir user_logs
-mkdir event_logs
+mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 #one more change
