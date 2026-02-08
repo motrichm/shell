882c14eac953d3629ba27a6b1aedd6e4748ef25f
Mari Motrich <mmotrich@adm.utoronto.ca>
2026-02-08 09:04:00 -0500

    ome more time fixing issues

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -50,22 +50,22 @@ mkdir event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
-cp ./data/raw/*server*.log ./data/processed/server_logs/
+cp  /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/raw/server*.log /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/*user*.log ./data/processed/user_logs/
+
+cp  /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/raw/*user*.log /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed/user_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
-# Remove from ./data/raw
-rm ./data/raw/*ipaddr*
+rm /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/raw/*ipaddr*
 
 # Remove from ./data/processed/user_logs
-rm ./data/processed/user_logs/*ipaddr*
+rm /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
-find ./data/processed -type f > ./data/inventory.txt
+find /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/processed -type f > /e/DeployingAIMicrocredential/shell/02_activities/assignments/newproject/data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
